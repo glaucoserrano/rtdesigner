693111f4ab5a4e50b43202497623525e3204140d
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-05-26 15:00:01 -0300

    fix: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { AboutSection } from "./components/aboutsection";
 import { DetailsSection } from "./components/detailssection/";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
 
 
 import './styles/globals.css';
@@ -7,8 +9,10 @@ import './styles/globals.css';
 export default function Home() {
   return (
     <>
+      <Header />
       <AboutSection />
       <DetailsSection />
+      <Footer />
     </>
      
   );
